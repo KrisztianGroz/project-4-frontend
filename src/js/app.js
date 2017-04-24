adc04529b73d6d0a4efa5bf5d9ea49cfc96622ca
Krisztian Groz <krisztian.groz@gmail.com>
2017-04-24 12:04:30 +0100

    Updating bower.json

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 // app.js is the main JS file which you should define your Angular module
 angular
-.module('robocombat', ['ui.router', 'ngResource', 'satellizer', 'checklist-model'])
+.module('robocombat', ['ui.router', 'ngResource', 'satellizer', 'checklist-model','ui.bootstrap', 'ngAnimate'])
 .constant('API_URL', 'http://localhost:3000/api');
